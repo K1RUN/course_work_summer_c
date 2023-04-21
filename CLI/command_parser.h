cd58bf62028268a6f4056d59015723743216e77a
K1RUN <syzdykov.nurgalym@gmail.com>
2023-04-22 00:24:30 +0300

    CLI interface draft

--- CLI/command_parser.h
+++ CLI/command_parser.h
@@ -1,4 +1,9 @@
 #ifndef MY_COMMAND_PARSER_H
 #define MY_COMMAND_PARSER_H
-
+#include "getopt.h"
+#include "commands.h"
+#include "helpmenu.h"
+#include "../Image/bmp_parser.h"
+#include <stddef.h>
+int parse_commands(int argc, char* argv[]);
 #endif //MY_COMMAND_PARSER_H
