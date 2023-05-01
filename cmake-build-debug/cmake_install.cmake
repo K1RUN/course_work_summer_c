93a6e92ff33a1984dc73c0890548a6467da70785
K1RUN <syzdykov.nurgalym@gmail.com>
2023-05-01 11:30:46 +0300

    nothing

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/samsepi/Documents/code/C/course_work/spring/my
+# Install script for directory: /home/samsepi/Documents/code/C/course_work/spring/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/samsepi/Documents/code/C/course_work/spring/my/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/samsepi/Documents/code/C/course_work/spring/src/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
